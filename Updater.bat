71facada731a287a0b2ecba825e59337e158e538
LPDMinecraft <l_jost@online.de>
2022-05-04 21:16:00 +0200

    5.0.3

--- Updater.bat
+++ Updater.bat
@@ -2,21 +2,26 @@
 goto menu
 
 :menu
+del Update.txt
 echo Do you want to update?
 echo (J) Yes (N) No
 set /p a=Update: 
 if "%a%"=="y" goto create
+if "%a%"=="j" goto create
+if "%a%"=="Y" goto create
+if "%a%"=="J" goto create
 cd ./data
 echo No > Update.txt
 call ./MCServerCreator.bat
 
 :create
-curl -o Files.zip "https://github.com/Lcraft-Developers/Server-Creator-V2/archive/refs/heads/main.zip"
-tar -xf Files.zip
+curl -LJO https://github.com/Lcraft-Developers/Server-Creator-V2/archive/refs/heads/main.zip
+tar -xf Server-Creator-V2-main.zip
 echo Enter N
-xcopy ".\Server-Creator-main\*.*" ".\" /-Y /E
-del "Files.zip" /s /q /f
+xcopy ".\Server-Creator-V2-main\*.*" ".\" /-Y /E
+del "Server-Creator-V2-main.zip" /s /q /f
 del ".gitattributes" /s /q /f
-del "Server-Creator-main" /s /q /f
-FOR /D %%p IN (".\Server-Creator-main\*.*") DO rmdir "%%p" /s /q
-rmdir "Server-Creator-main" /s /q
+del "Server-Creator-V2-main" /s /q /f
+FOR /D %%p IN (".\Server-Creator-V2-main\*.*") DO rmdir "%%p" /s /q
+rmdir "Server-Creator-V2-main" /s /q
+pause
